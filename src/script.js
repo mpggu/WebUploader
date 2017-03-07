28c398e3cfd8f1488d61e59860ac249ffd00112d
Manuel Kraus <manuel@kraus.rocks>
2017-03-07 17:53:54 +0100

    Sometimes jQuery is the only way.. (IE8 compability)

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function storePassword(password) {
-  if (typeof Storage === 'undefined') {
+  if (typeof localStorage === 'undefined') {
     return;
   }
 
@@ -7,7 +7,7 @@ function storePassword(password) {
 }
 
 function getPassword() {
-  if (typeof Storage === 'undefined') {
+  if (typeof localStorage === 'undefined') {
     return '';
   }
 
@@ -15,61 +15,64 @@ function getPassword() {
 }
 
 function uploadFile(file) {
-  if (window.File && window.FileReader && window.FileList && window.Blob) {
-    if (!file) {
-      return alert('Datei konnte nicht geladen werden!');
-    }
-
-    var auth = prompt('Passwort bitte!', getPassword());
-
-    var r = new FileReader();
-    r.onload = function(e) {
-      var contents = e.target.result;
-      
-      var http = new XMLHttpRequest();
-      var url = 'http://sudocode.me:6767/api/v1/vplan';
-
-      http.onreadystatechange = function() { 
-        if (http.readyState === 4 && http.status !== 200) {
-          console.log(http);
-          return alert('Fehler beim Hochladen der Datei: ' + http.statusText);
-        } else if (http.readyState === 4 && http.status === 200) {
-          alert('Erfolgreich hochgeldaden');
-          if (!getPassword()) {
-            storePassword(auth);
-          }
-        }
-      }
-
-      http.open('POST', url, true);
-      http.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-      http.setRequestHeader('Authorization', auth);
-
-      http.send(JSON.stringify({
-        vplan: contents
-      }));
-
-      document.getElementById('fileinput').value = "";
-    }
-    return r.readAsText(file, 'ISO-8859-1');
+  if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
+    return;
   }
+
+  if (!file) {
+    return alert('Datei konnte nicht geladen werden!');
+  }
+
+  var auth = prompt('Passwort bitte!', getPassword());
+
+  var r = new FileReader();
+  r.onload = function (e) {
+    var content = e.target.result;
+    var url = 'http://localhost:6767/api/v1/vplan';
+
+    $.ajax({
+      url: url,
+      data: {
+        vplan: content
+      },
+      error: function(xhr, statusText) {
+        alert('Datei konnte nicht hochgeladen werden: ' + xhr.status + ', ' + statusText)
+      },
+      success: function() {
+        if (!getPassword()) {
+          storePassword(auth);
+        }
+        alert('Datei erfolgreich hochgeladen!');
+      },
+      headers: {
+        Authorization: auth
+      },
+      type: 'POST',
+    })
+
+    $('#fileinput').value = '';
+  }
+
+  return r.readAsText(file, 'ISO-8859-1');
+  
 }
 
-document.getElementById('fileinput').addEventListener('change', function(evt) {
-  uploadFile(evt.target.files[0]); 
-}, false);
+$(function() {
+  $('#fileinput')[0].addEventListener('change', function(evt) {
+    uploadFile(evt.target.files[0]); 
+  });
 
+  var fileDropper = $('.wrapper')[0];
 
-var fileDropper = document.getElementsByClassName('wrapper')[0];
+  fileDropper.addEventListener('dragover', function (evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
+  });
 
-fileDropper.addEventListener('dragover', function (evt) {
-  evt.stopPropagation();
-  evt.preventDefault();
-  evt.dataTransfer.dropEffect = 'copy';
-});
-
-fileDropper.addEventListener('drop', function(evt) {
-  evt.stopPropagation();
-  evt.preventDefault();
-  uploadFile(evt.dataTransfer.files[0]);
+  fileDropper.addEventListener('drop', function(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    uploadFile(evt.dataTransfer.files[0]);
+  });
 });
